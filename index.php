ab9ae9faced77b2948fb9a99e567505f01942723
rianasantatra <rianasantatra665@gmail.com>
2022-10-26 14:12:54 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Riana Santatra</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" type="text/css" media="screen" href="vendor/css/css/css.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="vendor/css/css/app.css" />
+</head>
+
+<body data-spy="scroll" data-target="#navigation">
+
+    <nav class="navbar navbar-default navbar-fixed-top" id="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about">Moi</a></li>
+                    <li><a href="#skills">Compétences</a></li>
+                    <li><a href="#experiences">Expériences</a></li>
+                    <li><a href="#educations">Formations</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#recommandations">Recommandations</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+    </nav>
+
+    <section id="about" class="container-fluid">
+        <div class="col-xs-8 col-md-4 profile-pic">
+            <img src="vendor/img/pdp.jpg" alt="profile-pic" class="img-circle" />
+        </div>
+        <div class="heading">
+            <h1>Bonjour, Je suis Riana Santatra</h1>
+            <h3>Développeur Web</h3>
+            <a href="vendor/doc/cv.pdf" class="download">Télecharger le CV</a>
+        </div>
+    </section>
+
+    <section id="skills">
+        <div class="red-divider"></div>
+        <div class="heading">
+            <h2>Compétences</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="85" aria-valuemax="100" aria-valuemin="0" style="width: 90%">
+                            <h5>HTML 90%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="75" aria-valuemax="100" aria-valuemin="0" style="width: 80%">
+                            <h5>CSS 80%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 75%">
+                            <h5>Photoshop 75%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 65%">
+                            <h5>Illustrator 65%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 80%">
+                            <h5>Système Windows 80%</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="85" aria-valuemax="100" aria-valuemin="0" style="width: 75%">
+                            <h5>PHP 75%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="75" aria-valuemax="100" aria-valuemin="0" style="width: 85%">
+                            <h5>MYSQL 85%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 55%">
+                            <h5>JAVA 55%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 55%">
+                            <h5>C++ 55%</h5>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="55" aria-valuemax="100" aria-valuemin="0" style="width: 65%">
+                            <h5>Système Linux 65%</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="experiences">
+        <div class="white-divider"></div>
+        <div class="heading">
+            <h2>Expériences</h2>
+        </div>
+        <ul class="timeline">
+            <li>
+                <div class="timeline-badge">
+                    <span class="glyphicon glyphicon-briefcase"></span>
+                </div>
+                <div class="timeline-panel-container">
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h3 class="text-uppercase">paositra malagasy</h3>
+                            <h4>Agent complément d’effectif</h4>
+                            <p class="text-muted">
+                                <small class="glyphicon glyphicon-time"></small> Avr. 2017 -
+                                maintenant
+                            </p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Maintenance hardware et softwae</p>
+                            <p>Dépannage réseaux</p>
+                            <p>
+                                Installations et déploiment des applications de l'entreprise
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge">
+                    <span class="glyphicon glyphicon-briefcase"></span>
+                </div>
+                <div class="timeline-panel-container-inverted">
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h3 class="text-uppercase">trusty labs</h3>
+                            <h4>Stagiaire</h4>
+                            <p class="text-muted">
+                                <small class="glyphicon glyphicon-time"></small> Jan. – Mar.
+                                2017
+                            </p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Développement des nouveaux applications web</p>
+                            <p>Maintenance des applications web éxistantes</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge">
+                    <span class="glyphicon glyphicon-briefcase"></span>
+                </div>
+                <div class="timeline-panel-container">
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h3 class="text-uppercase">paositra malagasy</h3>
+                            <h4>Stagiaire</h4>
+                            <p class="text-muted">
+                                <small class="glyphicon glyphicon-time"></small> Jan. – Mar.
+                                2016
+                            </p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Développement d'application intranet</p>
+                            <p>Dépannage réseaux</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge">
+                    <span class="glyphicon glyphicon-briefcase"></span>
+                </div>
+                <div class="timeline-panel-container-inverted">
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h3 class="text-uppercase">blanche birger sarl</h3>
+                            <h4>Stagiaire</h4>
+                            <p class="text-muted">
+                                <small class="glyphicon glyphicon-time"></small> Sept. – Nov.
+                                2014
+                            </p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>administration réseaux sous CISCO</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </section>
+    <section id="educations">
+        <div class="red-divider"></div>
+        <div class="heading">
+            <h2>Formations</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="education-block">
+                        <h5>2017</h5>
+                        <span class="glyphicon glyphicon-education"></span>
+                        <h3>ISM-Advancea</h3>
+                        <h4>Licence informatique</h4>
+                        <div class="red-divider"></div>
+                        <p>Option design et développement web</p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="education-block">
+                        <h5>2016</h5>
+                        <span class="glyphicon glyphicon-education"></span>
+                        <h3>ISM-Advancea</h3>
+                        <h4>DTS informatique</h4>
+                        <div class="red-divider"></div>
+                        <p>Option design et développement web</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio">
+        <div class="white-divider"></div>
+        <div class="heading">
+            <h2>PORTFOLIO</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/google_translate.png" alt="google">
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/facebook_share.png" alt="facebook">
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/twitter_retweet.png" alt="twitter">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/youtube.png" alt="google">
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/facebook_video.png" alt="facebook">
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="#" class="thumbnail">
+                        <img src="vendor/img/twitter_video.png" alt="twitter">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="recommandations">
+        <div class="red-divider"></div>
+        <div class="heading">
+            <h2>Recommandations</h2>
+        </div>
+        <div id="mycarousel" class="carousel slide text-center" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#mycarousel" data-slide-to="1"></li>
+                <li data-target="#mycarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <h3>"John t'es le meilleur! Merci pour tout..."</h3>
+                    <h4>Larry Page,Google Co-Founder</h4>
+                </div>
+                <div class="item">
+                    <h3>"Merci John de m'avoir appris à coder... Tout ça c'est grâce à toi!"</h3>
+                    <h4>Mark Zuckerberg,Facebook Founder and CEO</h4>
+                </div>
+                <div class="item">
+                    <h3>"L'esprit le plus créatif que j'ai vu de toute ma vie..."</h3>
+                    <h4>Jack Dorsey,Twitter Founder and CEO</h4>
+                </div>
+            </div>
+            <a href="#mycarousel" class="left carousel-control" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a href="#mycarousel" class="right carousel-control" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <section id="contact">
+        <div class="white-divider"></div>
+        <div class="heading">
+            <h2>Contact</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1">
+                    <form id="contact-form" role="form" method="post" action="">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="prénom">Prénom<span class=""> *</span></label>
+                                <input id="firstname" type="text" name="firstname" class="form-control" placeholder="Prénom">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="nom">Nom<span class=""> *</span></label>
+                                <input id="lastname" type="text" name="lastname" class="form-control" placeholder="Nom">
+                                <p class="comments"></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="email">Email<span class=""> *</span></label>
+                                <input id="email" type="text" name="email" class="form-control" placeholder="Email">
+                                <p class="comments"></p>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="phone">Téléphone<span class=""> *</span></label>
+                                <input id="phone" type="tel" name="phone" class="form-control" placeholder="Téléphone">
+                                <p class="comments"></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="message">Message<span class=""> *</span></label>
+                                <textarea id="message" name="message" class="form-control" placeholder="Message..." rows="4"></textarea>
+                                <p class="comments"></p>
+                            </div>
+                        </div>
+                        <div class=" row">
+                            <div class="col-md-12">
+                                <p class=""><strong>* informations requis</strong></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <input type="submit" class="button" value="Envoyer">
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center">
+        <a href="#about">
+            <span class="glyphicon glyphicon-chevron-up"></span>
+        </a>
+        <h5>© 2022 | rianasantatra.com</h5>
+    </footer>
+
+    <script src="vendor/js/jquery.js"></script>
+    <script src="vendor/js/app.js"></script>
+    <script src="vendor/js/script.js"></script>
+
+</body>
+
+</html>
